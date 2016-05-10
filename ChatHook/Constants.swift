74a743d955886dae487e3b33c6613c7e9e8d71ae
Kevin Farm <kfarm@splashscene.com>
2016-05-10 10:49:39 -0500

    Set up data services and imported introVideo

---
+++ ChatHook/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  driveby_Showcase
+//
+//  Created by Kevin Farm on 4/12/16.
+//  Copyright © 2016 splashscene. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
+let KEY_UID = "uid"
+let IMAGESHACK_URL = "https://post.imageshack.us/upload_api.php"
+let IMAGESHACK_API_KEY = "79DFGHIQadc4d7cdf7cc6bb81f0bc1cbac0b7237"
+
+//segues
+let SEGUE_LOGGED_IN = "loggedIn"
+
+//Status Codes
+let STATUS_ACCOUNT_NONEXIST = -8
+let STATUS_ACCOUNT_WRONGPASSWORD = -6
